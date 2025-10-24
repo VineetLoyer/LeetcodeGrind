158e9757491df8f43926c611ec986116762a3a91
Vineet Kumar Loyer <vloyer@usc.edu>
2025-10-24 08:59:56 -0700

    Time: 4 ms (38.76%), Space: 18.6 MB (37.8%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,29 +3,26 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows,cols = len(matrix), len(matrix[0])
+        rows,cols = len(matrix),len(matrix[0])
+        #placeholders for first row and first col
         first_row_zero = any(matrix[0][j]==0 for j in range(cols))
         first_col_zero = any(matrix[i][0]==0 for i in range(rows))
 
-        #mark zero - first iteration of matrix
+        #step2:traverse the matrix and set first row/col elements to 0 (first row and first col are placeholders)
         for i in range(1,rows):
             for j in range(1,cols):
                 if matrix[i][j]==0:
-                    matrix[i][0]=0 #mark the row 0
-                    matrix[0][j]=0 #mark the col 0
-
-        #post traveral - setting row and col to 0s
+                    matrix[0][j]=0
+                    matrix[i][0]=0
+        #step3: traverse the matrix and update the elements to 0, based on placeholders.
         for i in range(1,rows):
             for j in range(1,cols):
-                if matrix[i][0]==0 or matrix[0][j]==0:
+                if matrix[0][j]==0 or matrix[i][0]==0:
                     matrix[i][j]=0
-
-        
-        #handle first row and column
+        #lastly, make the first row/col to 0, based on flag.
         if first_row_zero:
             for j in range(cols):
                 matrix[0][j]=0
         if first_col_zero:
             for i in range(rows):
                 matrix[i][0]=0
-        

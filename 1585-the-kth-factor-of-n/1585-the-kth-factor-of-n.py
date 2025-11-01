1d5fec5438371c64895271226c58fbb784347d62
Vineet Kumar Loyer <vloyer@usc.edu>
2025-11-01 12:04:20 -0700

    Time: 0 ms (100%), Space: 18 MB (22.83%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.py
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.py
@@ -1,26 +1,13 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
-        # fa = [] #array to store factors
-        # for i in range(1,n+1):
-        #     if n%i==0:
-        #         fa.append(i)
-        # print(fa)
-        # if k<=len(fa):
-        #     return fa[k-1]
-        # else:
-        #     return -1
-        count = 0
-        for i in range(1,int(n**0.5)+1):
-            if n%i==0:
-                count+=1
-                if count==k:
-                    return i
         factors = []
-        for i in range(int(n**0.5),0,-1):
-            if n%i==0 and i!=n//i:
-                factors.append(n//i)
-        for factor in factors:
-            count+=1
-            if count==k:
-                return factor
-        return -1
+        large_factors = []
+        for i in range(1, int(n ** 0.5) + 1):
+            if n % i == 0:
+                factors.append(i)
+                if i != n // i:
+                    large_factors.append(n // i)
+        # Combine factors in correct order: small factors first, then reversed large factors
+        all_factors = factors + large_factors[::-1]
+        return all_factors[k-1] if k <= len(all_factors) else -1
+        

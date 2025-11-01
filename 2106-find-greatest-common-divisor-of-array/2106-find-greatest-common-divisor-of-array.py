293d0ec80d7f6766f1a6631d512f6b26799e9954
Vineet Kumar Loyer <vloyer@usc.edu>
2025-11-01 11:06:14 -0700

    Time: 1 ms (26.69%), Space: 17.8 MB (69.46%) - LeetHub

---
+++ 2106-find-greatest-common-divisor-of-array/2106-find-greatest-common-divisor-of-array.py
@@ -0,0 +1,9 @@
+def gcd(a,b):
+    while b:
+        a,b=b,a%b
+    return a
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        num1 = min(nums)
+        num2 = max(nums)
+        return gcd(num1,num2)

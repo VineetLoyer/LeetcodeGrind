ce70a1a61bb8452271fb34b48b987bf066d3653c
Vineet Kumar Loyer <vloyer@usc.edu>
2025-11-01 09:30:53 -0700

    Time: 37 ms (69.64%), Space: 18 MB (17.07%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,22 +1,35 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        rev_number = 0
-        sign = -1
-        if x<0:
-            x*=sign
-        else:
-            sign = 1
+        # rev_number = 0
+        # sign = -1
+        # if x<0:
+        #     x*=sign
+        # else:
+        #     sign = 1
         
-        while x>0:
-            last_digit = x % 10
-            if rev_number > 214748364 or (rev_number == 214748364 and last_digit > 7):
+        # while x>0:
+        #     last_digit = x % 10
+        #     if rev_number > 214748364 or (rev_number == 214748364 and last_digit > 7):
+        #         return 0
+        #     if rev_number < -214748364 or (rev_number == -214748364 and last_digit < -8):
+        #         return 0
+        #     rev_number = rev_number * 10 + last_digit
+        #     x = x // 10
+        
+        # if sign !=1:
+        #     rev_number*=-1
+        
+        # return rev_number
+
+        # In more articulate manner
+        rev_num = 0
+        sign = -1 if x<0 else 1
+        x = abs(x)
+        while x!=0:
+            last_digit = x%10
+            x=x//10
+            if rev_num > 214748364 or (rev_num == 214748364 and last_digit>7):
                 return 0
-            if rev_number < -214748364 or (rev_number == -214748364 and last_digit < -8):
-                return 0
-            rev_number = rev_number * 10 + last_digit
-            x = x // 10
-        
-        if sign !=1:
-            rev_number*=-1
-        
-        return rev_number
+            rev_num = rev_num *10 + last_digit
+        return rev_num*sign
+

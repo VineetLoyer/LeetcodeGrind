eff05afb89d79776ef34ecae6b6595e4cbfb2970
Vineet Kumar Loyer <vloyer@usc.edu>
2025-11-02 09:53:25 -0800

    Time: 0 ms (100%), Space: 21.2 MB (80.69%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,28 +1,32 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        #brute force, making a frequency dict and then converting to array and returning top k
+        # #brute force, making a frequency dict and then converting to array and returning top k
+        # # count={}
+        # # for num in nums:
+        # #     count[num]=1+count.get(num,0)
+        # # arr=[]
+        # # for num,cnt in count.items():
+        # #     arr.append([cnt,num])
+        # # arr.sort()
+        # # res=[]
+        # # while len(res)<k:
+        # #     res.append(arr.pop()[1])
+        # # return res
+
+        # # we can optimize this using bucket sort, in bucket sort we have frequencies as buckets, and each bucket has a set of numbers.
         # count={}
+        # freq=[[] for _ in range(len(nums)+1)]
         # for num in nums:
         #     count[num]=1+count.get(num,0)
-        # arr=[]
         # for num,cnt in count.items():
-        #     arr.append([cnt,num])
-        # arr.sort()
+        #     freq[cnt].append(num)
         # res=[]
-        # while len(res)<k:
-        #     res.append(arr.pop()[1])
-        # return res
-
-        # we can optimize this using bucket sort, in bucket sort we have frequencies as buckets, and each bucket has a set of numbers.
-        count={}
-        freq=[[] for _ in range(len(nums)+1)]
-        for num in nums:
-            count[num]=1+count.get(num,0)
-        for num,cnt in count.items():
-            freq[cnt].append(num)
-        res=[]
-        for i in range(len(freq)-1,0,-1):
-            for num in freq[i]:
-                res.append(num)
-                if len(res)==k:
-                    return res
+        # for i in range(len(freq)-1,0,-1):
+        #     for num in freq[i]:
+        #         res.append(num)
+        #         if len(res)==k:
+        #             return res
+        freq = Counter(nums)
+        # Sort unique elements based on count (descending)
+        sorted_unique = sorted(freq, key=lambda x: -freq[x])
+        return sorted_unique[:k]

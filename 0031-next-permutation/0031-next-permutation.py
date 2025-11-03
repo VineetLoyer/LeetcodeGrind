4e8d8ad889b6a06faf3137f50abc852a057698d9
Vineet Kumar Loyer <vloyer@usc.edu>
2025-11-03 09:07:23 -0800

    Time: 0 ms (100%), Space: 17.7 MB (56.91%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,14 +1,24 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        n = len(nums)
-        i = n-2
+        i = len(nums)-2
         while i>=0 and nums[i]>=nums[i+1]:
             i-=1
         if i>=0:
-            j=n-1
+            j = len(nums)-1
             while nums[j]<=nums[i]:
                 j-=1
-            nums[i],nums[j] = nums[j],nums[i]
-        nums[i+1:] = reversed(nums[i+1:])
-        return nums
+            nums[j],nums[i]=nums[i],nums[j]
+        nums[i+1:]=reversed(nums[i+1:])
         
+
+        # nums = [1,2,3]
+        # i = len(nums)-2 = 1
+        # i>=0 (yes) and nums[1] = 2 > nums[2]=3 No
+        
+        # i=1
+        # j = 2
+        # nums[j] (3)<nums[i](2) No
+        # Swap [1,3,2]
+
+        # nums[2+] = [1,3,2]
+

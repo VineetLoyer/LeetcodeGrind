d3dd4816f2ffa8da5f1471db3d6201e4dacb7a8e
Vineet Kumar Loyer <vloyer@usc.edu>
2025-11-06 08:39:56 -0800

    Time: 335 ms (80.64%), Space: 101.3 MB (96.32%) - LeetHub

--- 3863-power-grid-maintenance/3863-power-grid-maintenance.py
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.py
@@ -1,52 +1,52 @@
-from collections import defaultdict
-from bisect import bisect_left, insort
-from typing import List
-
-class UnionFind:
-    def __init__(self, n):
-        self.parent = list(range(n+1))
-    def find(self, x):
-        while self.parent[x] != x:
-            self.parent[x] = self.parent[self.parent[x]]
-            x = self.parent[x]
-        return x
-    def union(self, x, y):
-        self.parent[self.find(y)] = self.find(x)
+from collections import defaultdict, deque
+import heapq
 
 class Solution:
-    def processQueries(self, c: int, connections: List[List[int]], queries: List[List[int]]) -> List[int]:
-        uf = UnionFind(c)
+    def processQueries(self, c, connections, queries):
+        # 1. Build graph
+        graph = defaultdict(list)
         for u, v in connections:
-            uf.union(u, v)
+            graph[u].append(v)
+            graph[v].append(u)
         
-        # Group all stations by their root (component id)
-        comp_to_nodes = defaultdict(list)
-        for i in range(1, c+1):
-            root = uf.find(i)
-            comp_to_nodes[root].append(i)
+        # 2. Component identification via BFS
+        comp_id = [-1] * (c + 1)  # comp_id[i] == id of component containing i
+        components = []
+        curr_id = 0
+        for i in range(1, c + 1):
+            if comp_id[i] == -1:
+                q = deque([i])
+                comp = []
+                comp_id[i] = curr_id
+                while q:
+                    node = q.popleft()
+                    comp.append(node)
+                    for nei in graph[node]:
+                        if comp_id[nei] == -1:
+                            comp_id[nei] = curr_id
+                            q.append(nei)
+                heapq.heapify(comp)  # min-heap per component
+                components.append(comp)
+                curr_id += 1
         
-        # For each component, keep a sorted list of online stations
-        from sortedcontainers import SortedList  # for very efficient removal and min finding
-        comp_online = {}
-        for comp, nodes in comp_to_nodes.items():
-            comp_online[comp] = SortedList(nodes)
-        
-        offline = set()
-        res = []
+        # 3. Online/offline status array
+        offline = [False] * (c + 1)
+
+        # 4. Query processing
+        ans = []
         for t, x in queries:
-            root = uf.find(x)
-            online = comp_online[root]
+            cid = comp_id[x]
+            comp_heap = components[cid]
             if t == 2:
-                if x not in offline:
-                    idx = online.bisect_left(x)
-                    if idx < len(online) and online[idx] == x:
-                        online.pop(idx)
-                    offline.add(x)
+                offline[x] = True
             else:
-                if x not in offline:
-                    res.append(x)
-                elif online:
-                    res.append(online[0])
+                if not offline[x]:
+                    ans.append(x)
                 else:
-                    res.append(-1)
-        return res
+                    while comp_heap and offline[comp_heap[0]]:
+                        heapq.heappop(comp_heap)
+                    if comp_heap:
+                        ans.append(comp_heap[0])
+                    else:
+                        ans.append(-1)
+        return ans

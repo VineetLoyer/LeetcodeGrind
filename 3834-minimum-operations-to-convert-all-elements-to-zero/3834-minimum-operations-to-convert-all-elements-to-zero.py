3751209f6d4756e50ca6e4987c397e75ad79cfbf
Vineet Kumar Loyer <vloyer@usc.edu>
2025-11-09 20:47:16 -0800

    Time: 223 ms (53.75%), Space: 29.7 MB (90.63%) - LeetHub

--- 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.py
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/3834-minimum-operations-to-convert-all-elements-to-zero.py
@@ -1,32 +1,13 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 0:
-            return 0
-
-        # Nearest strictly smaller to the left
-        left_less = [-1] * n
-        st = []
-        for i, x in enumerate(nums):
-            while st and nums[st[-1]] >= x:  # pop >= to ensure "strictly smaller"
-                st.pop()
-            left_less[i] = st[-1] if st else -1
-            st.append(i)
-
-        # Nearest strictly smaller to the right
-        right_less = [n] * n
-        st = []
-        for i in range(n - 1, -1, -1):
-            x = nums[i]
-            while st and nums[st[-1]] >= x:  # pop >= for "strictly smaller"
-                st.pop()
-            right_less[i] = st[-1] if st else n
-            st.append(i)
-
-        # For each value a>0, count distinct (L,R) among indices with value a
-        buckets = defaultdict(set)  # value -> set of (L,R)
-        for i, x in enumerate(nums):
-            if x > 0:
-                buckets[x].add((left_less[i], right_less[i]))
-
-        return sum(len(s) for s in buckets.values())
+        s= []
+        res = 0
+        for a in nums:
+            while s and s[-1]>a:
+                s.pop()
+            if a == 0:
+                continue
+            if not s or s[-1]<a:
+                res+=1
+                s.append(a)
+        return res

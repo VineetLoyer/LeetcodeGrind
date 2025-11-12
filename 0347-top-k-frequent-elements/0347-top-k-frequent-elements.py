d6db470abcc1c4ef4e63ab868c2c8bc47cba0816
Vineet Kumar Loyer <vloyer@usc.edu>
2025-11-12 09:19:47 -0800

    Time: 2 ms (92.19%), Space: 21.4 MB (28.07%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,13 +2,5 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         #Top - k ? Buckets
         freq = Counter(nums)
-        n = len(nums)
-        buckets = [[] for _ in range(n+1)]
-        for nums,c in freq.items():
-            buckets[c].append(nums)
-        ans = []
-        for f in range(n,0,-1):
-            for num in buckets[f]:
-                ans.append(num)
-                if len(ans)==k:
-                    return ans
+        sorted_unique = sorted(freq, key=lambda x: -freq[x])
+        return sorted_unique[:k]

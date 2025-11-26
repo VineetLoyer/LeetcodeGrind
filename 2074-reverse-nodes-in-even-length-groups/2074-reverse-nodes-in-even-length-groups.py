784634ca46fa1cbe9fa1cc49c1c88d3088dbb255
Vineet Kumar Loyer <vloyer@usc.edu>
2025-11-26 11:47:16 -0800

    Time: 110 ms (52.36%), Space: 40.6 MB (51.35%) - LeetHub

--- 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.py
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.py
@@ -4,50 +4,52 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseLinkedList(self,head,k):
-        prev = None
-        curr = head
-        count = 0
-        while curr and count < k:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-            count += 1
-        # prev is new head of the reversed group, head is now the tail
-        # curr is the next group's start
-        return prev, head, curr
+    # def reverseLinkedList(self,head,k):
+    #     prev = None
+    #     curr = head
+    #     count = 0
+    #     while curr and count < k:
+    #         nxt = curr.next
+    #         curr.next = prev
+    #         prev = curr
+    #         curr = nxt
+    #         count += 1
+    #     # prev is new head of the reversed group, head is now the tail
+    #     # curr is the next group's start
+    #     return prev, head, curr
 
 
     def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode(0)
-        dummy.next = head
-        length = 0
-        tmp = head
-        while tmp:
-            length += 1
-            tmp = tmp.next
-        
+        # Edge case: 1 or 2 nodes only
+        if not head or not head.next or not head.next.next:
+            return head
+        node = head
         group = 1
-        curr = head
-        prevGroupTail = dummy
-        while curr:
-            groupSize = group
-            actual_count = 0
-            temp = curr
-            while temp and actual_count < groupSize:
-                temp = temp.next
-                actual_count+=1
-            if actual_count % 2==0:
-                revHead, revTail, nextGroupHead = self.reverseLinkedList(curr, groupSize)
-                prevGroupTail.next = revHead
-                revTail.next = nextGroupHead
-                prevGroupTail = revTail
-                curr = nextGroupHead
-            else:
-                # move prevGroupTail to curr, curr to group's end
-                for _ in range(actual_count):
-                    prevGroupTail = curr
-                    curr = curr.next
+
+        while node and node.next:
             group += 1
-        return dummy.next
+
+            # Count the number of nodes in the group
+            countNodes = 0
+            temp = node.next
+            while temp and countNodes < group:
+                temp = temp.next
+                countNodes += 1
+
+            # Reverse if countNodes is even, otherwise skip
+            if countNodes % 2 == 0:
+                curr = node.next
+                prev = None
+                for _ in range(countNodes):
+                    next_node = curr.next
+                    curr.next = prev
+                    prev = curr
+                    curr = next_node
+                tail = node.next
+                tail.next = curr
+                node.next = prev
+                node = tail
+            else:
+                for _ in range(countNodes):
+                    node = node.next
+        return head

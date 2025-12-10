8af11c4cb080f43991d623c9a3bdf4b4a49ab535
Vineet Kumar Loyer <vloyer@usc.edu>
2025-12-09 22:11:43 -0800

    Time: 7 ms (82.76%), Space: 18 MB (82.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,15 +3,14 @@ class Solution:
         left = 0
         right = len(s)-1
         while left<right:
-            if s[left].isalnum():
-                if s[right].isalnum():
-                    if s[left].lower()==s[right].lower():
-                        left+=1
-                        right-=1
-                    else:
-                        return False
-                else:
-                    right-=1
-            else:
+            while left<right and not s[left].isalnum():
                 left+=1
+            while left<right and not s[right].isalnum():
+                right-=1
+            
+            if s[left].lower()==s[right].lower():
+                left+=1
+                right-=1
+            else:
+                return False
         return True

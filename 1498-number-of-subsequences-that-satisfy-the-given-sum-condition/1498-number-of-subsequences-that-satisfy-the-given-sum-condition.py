113f76990f389b58a4cc518b02b61b8bbb93b151
Vineet Kumar Loyer <vloyer@usc.edu>
2025-12-11 15:01:18 -0800

    Time: 162 ms (58.47%), Space: 27.9 MB (56.64%) - LeetHub

---
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        left = 0
+        right = len(nums)-1
+        res=0
+        MOD = 10**9+7
+        while left<=right:
+            if nums[left]+nums[right]>target:
+                right-=1
+            else:
+                res += pow(2,right-left,MOD)
+                res%=MOD
+                left+=1
+        return res

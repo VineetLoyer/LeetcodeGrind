3d73cde0c4e9da9f40901db61588739f55bb3b02
Vineet Kumar Loyer <vloyer@usc.edu>
2025-12-10 23:30:51 -0800

    Time: 379 ms (69.03%), Space: 17.7 MB (81.49%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,20 +1,22 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        # use sorting and two pointers
-        #fix first two, and then use two pointer to find rest two elements to target sum.
-
         nums.sort()
-        n = len(nums)
         res = []
-        for i in range(n-3):
-            if i>0 and nums[i]==nums[i-1]: continue
-            for j in range(i+1,n-2):
-                if j>i+1 and nums[j]==nums[j-1]: continue
-                #now we have fixed two, find next two
-                left,right = j+1,n-1
+        for i in range(len(nums)-3):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            for j in range(i+1, len(nums)-2):
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                left = j+1
+                right = len(nums)-1
                 while left<right:
                     total = nums[i]+nums[j]+nums[left]+nums[right]
-                    if total==target:
+                    if total< target:
+                        left+=1
+                    elif total>target:
+                        right-=1
+                    else:
                         res.append([nums[i],nums[j],nums[left],nums[right]])
                         left+=1
                         right-=1
@@ -22,8 +24,4 @@ class Solution:
                             left+=1
                         while left<right and nums[right]==nums[right+1]:
                             right-=1
-                    elif total<target:
-                        left+=1
-                    else:
-                        right-=1
         return res

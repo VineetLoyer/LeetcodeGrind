be3ec7bf66727292319637838e6b02e715ac60b2
Vineet Kumar Loyer <vloyer@usc.edu>
2025-12-13 16:06:55 -0800

    Time: 0 ms (100%), Space: 17.8 MB (56.95%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -7,18 +7,10 @@ class Solution:
             j = len(nums)-1
             while nums[j]<=nums[i]:
                 j-=1
-            nums[j],nums[i]=nums[i],nums[j]
-        nums[i+1:]=reversed(nums[i+1:])
-        
-
-        # nums = [1,2,3]
-        # i = len(nums)-2 = 1
-        # i>=0 (yes) and nums[1] = 2 > nums[2]=3 No
-        
-        # i=1
-        # j = 2
-        # nums[j] (3)<nums[i](2) No
-        # Swap [1,3,2]
-
-        # nums[2+] = [1,3,2]
-
+            nums[i],nums[j] = nums[j],nums[i]
+        left = i+1
+        right = len(nums)-1
+        while left<right:
+            nums[left],nums[right]=nums[right],nums[left]
+            left+=1
+            right-=1

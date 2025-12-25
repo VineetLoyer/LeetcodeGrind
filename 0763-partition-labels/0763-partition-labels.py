4e86a7c6f1f0cbfa0d5716804a5bb428dadae620
Vineet Kumar Loyer <vloyer@usc.edu>
2025-12-25 11:35:56 -0800

    Time: 2 ms (94.54%), Space: 17.1 MB (98.5%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -3,13 +3,12 @@ class Solution:
         #hashmap to store last index of characters
         last_index = {char: i for i,char in enumerate(s) }
 
-        left = 0
-        right = 0
-        result = []
-        for i, char in enumerate(s):
-            right = max(right,last_index[char])
+        left=  0
+        right= 0
+        res = []
+        for i,char in enumerate(s):
+            right=max(right,last_index[char])
             if i==right:
-                current_partition_size = right-left+1
-                result.append(current_partition_size)
-                left = i+1
-        return result
+                res.append(right-left+1)
+                left=i+1
+        return res

1c346ad96c9760d8bee61fd87e03f3b0110d2d04
Vineet Kumar Loyer <vloyer@usc.edu>
2025-12-25 11:05:59 -0800

    Time: 3 ms (70.28%), Space: 17.5 MB (96.33%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.py
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -5,19 +5,16 @@ class Solution:
         right = len(tokens)-1
         score = 0
         max_score = 0
-        while left <= right:
-            #Option1: play face-up; if have enough power, buy the cheapest point(left)
-            if power>=tokens[left]:
+        while left<=right:
+            if tokens[left]<=power:
                 power-=tokens[left]
                 score+=1
                 left+=1
-                max_score = max(score,max_score)
-            #option2: Play face-down; if we need power and have score to spend, sell a point for max_power(right)
+                max_score=max(max_score,score)
             elif score>0:
                 power+=tokens[right]
                 score-=1
                 right-=1
-            #option3: stuck, can't buy score, can't buy power, game over.
             else:
                 break
         return max_score

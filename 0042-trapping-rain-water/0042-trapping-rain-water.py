d6fb63fcd1e794120f46803e5e6780c294c8bb26
Vineet Kumar Loyer <vloyer@usc.edu>
2025-12-25 11:58:35 -0800

    Time: 29 ms (21.42%), Space: 18.9 MB (98.35%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -10,13 +10,13 @@ class Solution:
         current_max = 0
         for i in range(n):
             current_max = max(current_max,height[i])
-            max_left[i] = current_max
+            max_left[i]=current_max
         
         #2. fill max_right
         current_max = 0
         for i in range(n-1,-1,-1):
             current_max = max(current_max,height[i])
-            max_right[i] = current_max
+            max_right[i]=current_max
         
         #3. calculate collected water
         total_water = 0
@@ -24,3 +24,14 @@ class Solution:
             water_at_i = min(max_left[i],max_right[i]) - height[i]
             total_water+=water_at_i
         return total_water
+
+#Ex: [4,2,0,3,2,5]
+# left = [4,4,4,4,4,5]
+# right = [5,5,5,5,5,5]
+# at i = min(4,5) - height[i] => 4-4=0
+# i=1, 4-2=2
+# i=1, 4-0=4
+# i=2, 4-3=1
+# i=3, 4-2=2
+# i=4, 5-5=0
+# total = 9

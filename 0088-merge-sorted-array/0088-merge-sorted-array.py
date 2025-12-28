9454879cdd271a2447ece7bb7f13da231e7cd64a
Vineet Kumar Loyer <vloyer@usc.edu>
2025-12-28 01:24:34 -0800

    Time: 0 ms (100%), Space: 17.5 MB (89.89%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -11,11 +11,11 @@ class Solution:
                 nums1[p]=nums1[p1]
                 p1-=1
             else:
-                nums1[p] = nums2[p2]
+                nums1[p]=nums2[p2]
                 p2-=1
             p-=1
         while p2>=0:
             nums1[p]=nums2[p2]
             p2-=1
             p-=1
-
+            

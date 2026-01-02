5c03faf3b6a4ef9447776f1e2e1b91e2c2cd4297
Vineet Kumar Loyer <vloyer@usc.edu>
2026-01-02 05:35:10 -0800

    Time: 0 ms (100%), Space: 17.5 MB (79.09%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -1,15 +1,17 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        nums1 = version1.split(".")
-        nums2 = version2.split(".")
+        lst1 = version1.split('.')
+        lst2 = version2.split('.')
 
-        n1,n2 = len(nums1),len(nums2)
-        for i in range(max(n1,n2)):
-            v1=int(nums1[i]) if i<n1 else 0
-            v2=int(nums2[i]) if i<n2 else 0
+        n = len(lst1)
+        m = len(lst2)
 
+        for i in range(max(n,m)):
+            v1 = int(lst1[i]) if i<n else 0
+            v2 = int(lst2[i]) if i<m else 0
             if v1<v2:
                 return -1
             elif v1>v2:
                 return 1
         return 0
+            
